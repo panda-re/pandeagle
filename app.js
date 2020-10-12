6569e3bb89c2088e74708a832a234dc24afc82c7
Fisher Shen <shenxinyu1999@outlook.com>
2020-10-12 16:28:28 -0400

    use original architecture, gave up pug

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const path = require('path');
 const app = express();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,29 +22,12 @@ pool.on('error', (err, client) => {
     console.error('Unexpected error on idle client', err);
 });
 
-app.get('/', (req, res) => {
+app.get('/app', (req, res) => {
     pool
         .query('SELECT * FROM Executions')
         .then((exe) => {
-            let exeID = exe.rows[0].execution_id;
-            pool
-                .query('SELECT * FROM Threads WHERE process_id IN (SELECT process_id FROM Processes WHERE execution_id = $1)', [exeID])
-                .then((thr) => {
-                    thr.rows.forEach(element => {
-                        element.name = element.names.join(' ');
-                    });
-                    res.render('index', {
-                        title: 'PANDeagle',
-                        executions: exe.rows,
-                        threads: thr.rows
-                    });
-                })
-                .catch((err) => {
-                    console.log(err);
-                    console.log("pool.idleCount: " + pool.idleCount);
-                    res.status(400);
-                    res.json(err);
-                });
+            res.status(200);
+            res.json(exe.rows);
         })
         .catch((err) => {
             console.log(err);
@@ -57,37 +37,38 @@ app.get('/', (req, res) => {
         });
 });
 
-app.get('/:executionName', (req, res) => {
-    console.log("HELLO");
+app.get('/defaultExecution', (req, res) => {
     pool
-        .query('SELECT * FROM Executions')
-        .then((exe) => {
-            pool
-                .query('SELECT * FROM Threads WHERE process_id IN (SELECT process_id FROM Processes WHERE execution_id = (SELECT execution_id FROM Executions WHERE name = $1))', [req.params.executionName])
-                .then((thr) => {
-                    thr.rows.forEach(element => {
-                        element.name = element.names.join(' ');
-                    });
-                    res.render('index', {
-                        title: 'PANDeagle',
-                        executions: exe.rows,
-                        threads: thr.rows
-                    });
-                })
-                .catch((err) => {
-                    console.log(err);
-                    console.log("pool.idleCount: " + pool.idleCount);
-                    res.status(400);
-                    res.json(err);
-                });
+        .query('SELECT * FROM Threads WHERE process_id IN (SELECT process_id FROM Processes WHERE execution_id = (SELECT execution_id FROM Executions LIMIT 1))')
+        .then((thr) => {
+            thr.rows.forEach(element => {
+                element.name = element.names.join(' ');
+            });
+            res.status(200);
+            res.json(thr.rows);
         })
         .catch((err) => {
-            console.log(err);
-            console.log("pool.idleCount: " + pool.idleCount);
             res.status(400);
             res.json(err);
         });
 });
+
+app.get('/:executionID', (req, res) => {
+    pool
+        .query('SELECT * FROM Threads WHERE process_id IN (SELECT process_id FROM Processes WHERE execution_id = $1)', [req.params.executionID])
+        .then((thr) => {
+            thr.rows.forEach(element => {
+                element.name = element.names.join(' ');
+            });
+            res.status(200);
+            res.json(thr.rows);
+        })
+        .catch((err) => {
+            res.status(400);
+            res.json(err);
+        });
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,39 @@
-function selectThread(thread_id) {
-    console.log(thread_id);
-    $.ajax({
-        url: 'plog_test',
-        type: 'GET',
-        dataType: 'JSON'
-    });
-
+function setup() {
+    getExecutions();
+    getThreads();
 }
+
+function getExecutions() {
+    $.ajax({
+        url: 'app',
+        type: 'GET',
+        dataType: 'JSON',
+        success: (executions) => {
+            executions.forEach(execution => {
+                $('#processSelect').append('<option value="' + execution.execution_id + '">' + execution.name + '</option>');
+            });
+        },
+        error: (xhr, status, err) => {
+            console.log(xhr);
+        }
+    });
+}
+
+function getThreads(executionID) {
+    $.ajax({
+        url: 'defaultExecution',
+        type: 'GET',
+        dataType: 'JSON',
+        success: (threads) => {
+            threads.forEach(thread => {
+                $('#threadSelect').append('<option value="' + thread.thread_id + '">' + thread.name + '</option>');
+            });
+        },
+        error: (xhr, status, err) => {
+            console.log(xhr);
+        }
+    });
+}
+
+
+$(window).on('load', setup);
