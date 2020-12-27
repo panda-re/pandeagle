ae71aa631a0075ae912c1231fcaa7f43eac4e49a
Valerie Liu <liur5@rose-hulman.edu>
2020-12-26 22:15:14 -0500

    add x axis

--- public/indexnew.js
+++ public/indexnew.js
@@ -1,21 +1,31 @@
 async function init() {
     const dataset = await d3.json('http://localhost:3000/executions/1/threadslices');
 
-    const svg = d3.select("body").append("svg").attr("width", 1250).attr("height", 600);
+    const dimensions = {
+        width: 1900,
+        height: 20 * dataset.length + 200,
+        xLength: 1500,
+        margin: {
+            top: 100,
+            left: 200,
+        },
+    }
+
+    const svg = d3.select("body").append("svg").attr("width", dimensions.width).attr("height", dimensions.height);
     
-    dotPlot(svg, dataset);
+    dotPlot(svg, dataset, dimensions);
 }
 
 document.addEventListener('DOMContentLoaded', init)
 
-function dotPlot(svg, dataset) {
+function dotPlot(svg, dataset, dimensions) {
 
     const idAccessor = data => +data["thread_id"];
     const nameAccessor = data => data["names"].join(" ");
     const sliceAccessor = data => data["thread_slices"]; 
 
     const bounds = svg.append("g")
-    .attr("transform", "translate(200, 100)");
+    .attr("transform", `translate(${dimensions.margin.left}, ${dimensions.margin.top})`);
 
     const yScale = d3.scaleBand()
     .domain(dataset.map(nameAccessor))
@@ -29,4 +39,19 @@ function dotPlot(svg, dataset) {
     .call(yAxisGenerator)
     .select(".domain")
     .attr("opacity", 0);
+
+    const maxTime = Math.max(...dataset.map(t => Math.max(...t.thread_slices.map(d => d.end_execution_offset))));
+    const minTime = Math.min(...dataset.map(t => Math.min(...t.thread_slices.map(d => d.end_execution_offset))));
+
+    const xScale = d3.scaleLinear()
+    .domain([minTime, maxTime])
+    .range([0, dimensions.xLength]);
+
+    const xAxisGenerator = d3.axisBottom()
+    .scale(xScale)
+    .tickSize(0);
+
+    bounds.append("g")
+    .call(xAxisGenerator)
+    .attr("transform", `translate(0,${dimensions.height-200})`)
 }
