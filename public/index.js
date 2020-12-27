2e8485968b05691a605ca8ee80d99b344a5866b4
Valerie Liu <liur5@rose-hulman.edu>
2020-12-27 00:01:41 -0500

    add slice to graph, initial prototype done

--- public/index.js
+++ public/index.js
@@ -1,184 +1,125 @@
-
 async function init() {
-  const data = await d3.json('./payloads/dataset.json');
+    const dataset = await d3.json('http://localhost:3000/executions/1/threadslices');
 
-  const body = document.querySelector('body');
-  const svg = DotPlot(data);
+    const dimensions = {
+        width: 1900,
+        lineHeight: 30,
+        height: 30 * dataset.length + 200,
+        xLength: 1500,
+        margin: {
+            top: 100,
+            left: 200,
+        },
+    }
 
-  body.appendChild(svg);
+    const svg = d3.select("body").append("svg").attr("width", dimensions.width).attr("height", dimensions.height);
+    
+    dotPlot(svg, dataset, dimensions);
 }
 
 document.addEventListener('DOMContentLoaded', init)
 
-function DotPlot(dataset) {
-  // attributes
-  const radiusCircles = 10;
-  const radiusAdults = 7;
+function renameDuplicates(threadNames) {
+    const nameCounts = new Map();
+    const newNames = [];
 
+    threadNames.forEach(name => {
+        let count = nameCounts.get(name); 
+        if (count == undefined) {
+            nameCounts.set(name, 1);
+            newNames.push(name);
+        } else {
+            count++;
+            nameCounts.set(name, count);
+            newNames.push(name + count.toString());
+        }
+    });
 
-  // Access Data
-  const xAccessorAA = d => +d["AllAdults"]
-  const xAccessorLV = d => +d["LikelyVoters"]
-  const xAccessorNV = d => +d["NonVoters"]
-  const yAccessor = d => d["Race"]
+    return newNames;
+}
 
-  // Chart dimensions
-  let dimensions = {
-    width: 1250,
-    height: 1000 * 0.334,
-    margin: {
-      top: 90,
-      right: 15,
-      bottom: 60,
-      left: 550,
-    },
-  }
-  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+function dotPlot(svg, dataset, dimensions) {
 
-  // Draw canvas
-  let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute('width', dimensions.width);
-  svg.setAttribute('height', dimensions.height);
-  svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
-  svg = d3.select(svg)
-  const bounds = svg.append("g")
-    .attr("transform", `translate(${dimensions.margin.left}, ${dimensions.margin.top})`)
+    let threadNames = dataset.map(data => data["names"].join(" "));
+    threadNames = renameDuplicates(threadNames);
 
-  // Scales
-  const xScale = d3.scaleLinear()
-    // .domain([0, d3.max(dataset, xAccessorLV)])
-    .domain([0, 1])
-    .range([0, dimensions.boundedWidth])
-    .nice()
+    for (let i = 0; i < dataset.length ; i++) {
+        dataset[i].newName = threadNames[i];
+    }
 
-  const yScale = d3.scaleBand()
-    .domain(dataset.map(yAccessor))
-    .range([0, dimensions.boundedHeight])
-  // .paddingInner(0.5)
-  // .paddingOuter(1.5)
+    const nameAccessor = data => data["newName"];
+    const startAccessor = data => data["thread_slices"].map(d => d.start_execution_offset);
+    const endAccessor = data => data["thread_slices"].map(d => d.end_execution_offset);
 
-  // Draw data
-  const lineGenerator = d3.line()
-  const axisLinePath = d => lineGenerator([[0, yScale(d) + 0.5], [dimensions.boundedWidth, yScale(d) + 0.5]])
-  const dotsLinePathNVAA = d => lineGenerator([[xScale(xAccessorNV(d)), 0], [xScale(xAccessorAA(d)), 0]])
-  const dotsLinePathAALV = d => lineGenerator([[xScale(xAccessorAA(d)), 0], [xScale(xAccessorLV(d)), 0]])
+    // create new graph
+    const graph = svg.append("g")
+    .attr("transform", `translate(${dimensions.margin.left}, ${dimensions.margin.top})`);
 
-  const dotsGroup = bounds.append("g")
-    .attr("class", "dots")
+    // y-axis
+    const yScale = d3.scaleBand()
+    .domain(threadNames)
+    .range([0, threadNames.length * dimensions.lineHeight]);
 
-  const dots = dotsGroup.selectAll("g")
-    .data(dataset)
-    .enter().append("g")
-    .attr("class", "dot")
-    .attr("transform", d => `translate(0, ${(yScale(yAccessor(d)) + (yScale.bandwidth() / 2))})`)
-
-  dots.append("path")
-    .attr("class", "dots-line-NVAA")
-    .attr("d", dotsLinePathNVAA)
-
-  dots.append("path")
-    .attr("class", "dots-line-AALV")
-    .attr("d", dotsLinePathAALV)
-
-  const nonVoterCircles = dots.append("circle")
-    .attr("class", "non-voters")
-    .attr("r", radiusCircles)
-    .attr("cx", d => xScale(xAccessorNV(d)))
-
-  const likelyVoterCircles = dots.append("circle")
-    .attr("class", "likely-voters")
-    .attr("r", radiusCircles)
-    .attr("cx", d => xScale(xAccessorLV(d)))
-
-  const allAdultCircles = dots.append("circle")
-    .attr("class", "all-adults")
-    .attr("r", radiusAdults)
-    .attr("cx", d => xScale(xAccessorAA(d)))
-
-  // Make axes
-  const xAxisGenerator = d3.axisBottom()
-    .scale(xScale)
-    .tickFormat(d3.format(".0%"))
-    .ticks(5)
-
-  const yAxisGenerator = d3.axisLeft()
+    const yAxisGenerator = d3.axisLeft()
     .scale(yScale)
-    .tickSize(0)
+    .tickSize(0);
 
-  const xAxis = bounds.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", `translate(0,${dimensions.boundedHeight})`)
-    .call(xAxisGenerator)
-
-  const yAxis = bounds.append("g")
-    .attr("class", "y-axis")
+    graph.append("g")
     .attr("transform", "translate(-20, 0)")
     .call(yAxisGenerator)
     .select(".domain")
-    .attr("opacity", 0)
+    .attr("opacity", 0);
 
-  // gridlines
-  const gridLines = bounds.append("g")
-    .attr("class", "grid-lines")
-  gridLines.selectAll("path")
+    // x-axis
+    const maxTime = Math.max(...dataset.map(t => Math.max(...t.thread_slices.map(d => d.end_execution_offset))));
+    const minTime = Math.min(...dataset.map(t => Math.min(...t.thread_slices.map(d => d.end_execution_offset))));
+
+    const xScale = d3.scaleLinear()
+    .domain([minTime, maxTime])
+    .range([0, dimensions.xLength]);
+
+    const xAxisGenerator = d3.axisBottom()
+    .scale(xScale)
+
+    graph.append("g")
+    .call(xAxisGenerator)
+    .attr("transform", `translate(0,${dimensions.height-dimensions.margin.top*2})`)
+
+    // gray axis line
+    const lineGenerator = d3.line()
+    const axisLine = d => lineGenerator([[0, yScale(d) + dimensions.lineHeight/2], [dimensions.xLength, yScale(d) + dimensions.lineHeight/2]])
+
+    graph.append("g")
+    .selectAll("path")
     .data(yScale.domain())
     .enter().append("path")
     .attr("class", "grid-line")
-    .attr("d", axisLinePath)
+    .attr("d", axisLine)
 
-  // text labels for the x and y axes
-  svg.append("text")
-    .attr("transform",
-      "translate(" + (dimensions.width / 2) + " ," +
-      (dimensions.height + dimensions.margin.top + 20) + ")")
-    .style("text-anchor", "middle")
-    .text("Date");
+    // slices
+    const slicePath = d => {
+        const startPoints = startAccessor(d);
+        const endPoints = endAccessor(d);
 
-  svg.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - dimensions.margin.left)
-    .attr("x", 0 - (dimensions.height / 2))
-    .attr("dy", "1em")
-    .style("text-anchor", "middle")
-    .text("Value");
+        const context = d3.path()
 
-  // legend
-  // const legendLabels = [
-  //   { "label": "Non Voters", class: "non-voters" },
-  //   { "label": "All Adults", class: "all-adults" },
-  //   { "label": "Likely Voters", class: "likely-voters" },
-  // ]
-  // const legendX = dimensions.boundedWidth / 2
-  // const legendY = dimensions.margin.top / 2
-  // const spaceBetween = dimensions.boundedWidth / 8
-  // const titleOffset = -dimensions.boundedWidth / 2
+        for (let i = 0 ; i < startPoints.length ; i++) {
+            context.moveTo(xScale(startPoints[i]),0);
+            context.lineTo(xScale(endPoints[i]),0);
+        }
 
-  // const legend = svg.append("g")
-  //   .attr("transform", `translate(${legendX}, ${legendY})`)
+        return context;        
+    }
 
-  // legend.append("g")
-  //   .attr("class", "title")
-  //   .append("text")
-  //   .attr("x", titleOffset + dimensions.margin.left)
-  //   .attr("y", -20)
-  //   .text("Pandeagle")
+    const slicesGroup = graph.append("g")
+    .attr("class", "dots")
 
-  // legend.selectAll("circle")
-  //   .data(legendLabels)
-  //   .enter().append("circle")
-  //   .attr("cx", (d, i) => spaceBetween * i + 180)
-  //   .attr("cy", 5)
-  //   .attr("r", 4)
-  //   .attr("class", d => d.class)
+    const slices = slicesGroup.selectAll("g")
+    .data(dataset)
+    .enter().append("g")
+    .attr("transform", d => `translate(0, ${(yScale(nameAccessor(d)) + (yScale.bandwidth() / 2))})`)
 
-  // legend.append("g")
-  //   .selectAll("text")
-  //   .data(legendLabels)
-  //   .enter().append("text")
-  //   .attr("x", (d, i) => spaceBetween * i + 190)
-  //   .attr("y", 10)
-  //   .text(d => d.label)
-
-  return svg.node()
+    slices.append("path")
+    .attr("class", "slice")
+    .attr("d", slicePath)
 }
