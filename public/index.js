3eae44f4826a2138b5437f8e0f259d82d796a6cc
Jerry Zheng <zhengj2@rose-hulman.edu>
2020-12-28 21:51:29 -0500

    fixed chart height

--- public/index.js
+++ public/index.js
@@ -1,21 +1,24 @@
 async function init() {
-  const dataset = await d3.json('http://localhost:3000/executions/1/threadslices');
+  const data = await d3.json('http://localhost:3000/executions/1/threadslices');
 
-  const dimensions = {
-    margin: {
-      top: 10,
-      right: 20,
-      bottom: 30,
-      left: 120
-    },
-    width: 1000,
-    height: 500,
+  const width = 1000
+  const height = data.length * 30
+  const margin = {
+    top: 10,
+    right: 20,
+    bottom: 30,
+    left: 120
   }
-  const { width, height, margin } = dimensions
 
-  const svg = d3.select("body").append("svg").attr('height', height).attr('width', width);
+  const svg = d3.select("body")
+    .append("svg")
+    .attr('height', height)
+    .attr('width', width);
 
-  dotPlot(svg, dataset, dimensions);
+  const dimensions = Object.assign({}, { margin }, { height, width })
+  console.log(dimensions)
+
+  dotPlot(svg, data, dimensions);
 }
 
 document.addEventListener('DOMContentLoaded', init)
@@ -39,13 +42,14 @@ function renameDuplicates(threadNames) {
   return newNames;
 }
 
-function dotPlot(svg, dataset, dimensions) {
+function dotPlot(svg, data, dimensions) {
   const { width, height, margin } = dimensions
-  let threadNames = dataset.map(data => data["names"].join(" "));
+
+  let threadNames = data.map(data => data["names"].join(" "));
   threadNames = renameDuplicates(threadNames);
 
-  for (let i = 0; i < dataset.length; i++) {
-    dataset[i].newName = threadNames[i];
+  for (let i = 0; i < data.length; i++) {
+    data[i].newName = threadNames[i];
   }
 
   const nameAccessor = data => data["newName"];
@@ -53,7 +57,7 @@ function dotPlot(svg, dataset, dimensions) {
   const endAccessor = data => data["thread_slices"].map(d => d.end_execution_offset);
 
   // create new graph
-  const graph = svg.append("g")
+  const chart = svg
 
   // y-axis
   const yScale = d3.scaleBand()
@@ -64,14 +68,14 @@ function dotPlot(svg, dataset, dimensions) {
     .scale(yScale)
     .tickSize(0);
 
-  graph.append('g')
+  chart.append('g')
     .attr('transform', `translate(${margin.left},0)`)
     .call(yAxisGenerator)
     .call(g => g.select('.domain').remove())
 
   // x-axis
-  const maxTime = Math.max(...dataset.map(t => Math.max(...t.thread_slices.map(d => d.end_execution_offset))));
-  const minTime = Math.min(...dataset.map(t => Math.min(...t.thread_slices.map(d => d.end_execution_offset))));
+  const maxTime = Math.max(...data.map(t => Math.max(...t.thread_slices.map(d => d.end_execution_offset))));
+  const minTime = Math.min(...data.map(t => Math.min(...t.thread_slices.map(d => d.end_execution_offset))));
 
   const xScale = d3.scaleLinear()
     .domain([minTime, maxTime])
@@ -80,16 +84,16 @@ function dotPlot(svg, dataset, dimensions) {
   const xAxisGenerator = d3.axisBottom()
     .scale(xScale)
 
-  graph.append("g")
+  chart.append("g")
     .call(xAxisGenerator)
+    .attr('class', 'x-axis')
     .attr("transform", `translate(0,${dimensions.height - dimensions.margin.bottom})`)
 
   // gray axis line
   const lineGenerator = d3.line()
   const axisLine = d => lineGenerator([[margin.left, yScale(d) + yScale.bandwidth() / 2], [width - margin.right, yScale(d) + yScale.bandwidth() / 2]])
 
-
-  graph.append("g")
+  chart.append("g")
     .selectAll("path")
     .data(yScale.domain())
     .join('path')
@@ -111,11 +115,11 @@ function dotPlot(svg, dataset, dimensions) {
     return context;
   }
 
-  const slicesGroup = graph.append("g")
+  const slicesGroup = chart.append("g")
     .attr("class", "dots")
 
   const slices = slicesGroup.selectAll("g")
-    .data(dataset)
+    .data(data)
     .enter().append("g")
     .attr("transform", d => `translate(0, ${(yScale(nameAccessor(d)) + (yScale.bandwidth() / 2))})`)
 
@@ -133,8 +137,14 @@ function dotPlot(svg, dataset, dimensions) {
 
   function brushed({ selection }) {
     if (selection) {
-      svg.property('value', selection.map(xScale.invert, xScale))
-      svg.dispatch('input')
+      const focus = selection.map(xScale.invert).map(Math.floor)
+      console.log(focus)
+    }
+  }
+
+  function brushended({ selection }) {
+    if (!selection) {
+      xScale
     }
   }
 
